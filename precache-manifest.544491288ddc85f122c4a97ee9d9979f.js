0534c63a42357e71d49293a6ff57e566dec6d93c
Anh Minh <47974381+anhminh10a2hoa@users.noreply.github.com>
2020-06-15 20:22:59 +0300

    Updates

---
+++ precache-manifest.544491288ddc85f122c4a97ee9d9979f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c951171a4ca67fc7bbcd",
+    "url": "/Resume-React/static/css/main.39105696.chunk.css"
+  },
+  {
+    "revision": "c951171a4ca67fc7bbcd",
+    "url": "/Resume-React/static/js/main.c951171a.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/Resume-React/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "1e8ccb53b159f3156d63",
+    "url": "/Resume-React/static/js/runtime~main.1e8ccb53.js"
+  },
+  {
+    "revision": "270509b74932d2a5f8fe9666520aae03",
+    "url": "/Resume-React/index.html"
+  }
+];
