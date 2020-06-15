2abf5f0545c9d545b4d56917792452bfc8bf17c0
Anh Minh <47974381+anhminh10a2hoa@users.noreply.github.com>
2020-06-15 20:04:55 +0300

    Update

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 export default class Testimonials extends Component {
   render() {
     let resumeData = this.props.resumeData;
@@ -7,30 +7,40 @@ export default class Testimonials extends Component {
         <div className="text-container">
           <div className="row">
             <div className="two columns header-col">
-              <h1><span>Projects</span></h1>
+              <h1>
+                <span>Projects</span>
+              </h1>
             </div>
             <div className="ten columns flex-container">
               <div className="flexslider">
                 <ul className="slides">
-                  {
-                    resumeData.testimonials && resumeData.testimonials.map((item)=>{
-                      return(
+                  {resumeData.testimonials &&
+                    resumeData.testimonials.map((item) => {
+                      return (
                         <li>
-                          <img src={item.image} class="card-img-top" height="200px" width="400px"></img>
+                          <img
+                            src={item.image}
+                            class="card-img-top"
+                            height="200px"
+                            width="400px"
+                          />
                           <blockquote>
                             <a href={item.link}>Check it out: </a>
                             <cite>{item.name}</cite>
                           </blockquote>
                         </li>
-                      )
-                    })
-                  }
+                      );
+                    })}
                 </ul>
-              </div> {/* div.flexslider ends */}
-            </div> {/* div.flex-container ends */}
-          </div> {/* row ends */}
-        </div>  {/* text-container ends */}
+              </div>{" "}
+              {/* div.flexslider ends */}
+            </div>{" "}
+            {/* div.flex-container ends */}
+          </div>{" "}
+          {/* row ends */}
+        </div>{" "}
+        {/* text-container ends */}
       </section>
-        );
+    );
   }
 }
--- src/resumeData.js
+++ src/resumeData.js
@@ -1,126 +1,163 @@
 let resumeData = {
-    "imagebaseurl": "https://rbhatia46.github.io/",
-    "name": "Hoang Anh Minh",
-    "role": "Software developer",
-    "gmail": "hoanganhminhhn17@gmail.com",
-    "dob": "25/06/2000",
-    "nationality" : "Vietnamese",
-    "address" : "65200 Vaasa, Finland",
-    "linkedin": "https://www.linkedin.com/in/minh-hoang-110510181/",
-    "phonenumber": "0403620608",
-    "roleDescription": "",
-    "socialLinks": [{
-            "name": "linkedin",
-            "url": "https://www.linkedin.com/in/minh-hoang-110510181/",
-            "className": "fab fa-linkedin"
-        },
-        {
-            "name": "github",
-            "url": "https://github.com/anhminh10a2hoa",
-            "className": "fab fa-github"
-        },
-        {
-            "name": "skype",
-            "url": "https://www.facebook.com/anhminh.hoang.560",
-            "className": "fab fa-facebook-square"
-        }
-    ],
-    "aboutme": "Experienced IT developer with a demonstrated history of working in the software industry. Skilled in HTML, CSS, PHP, Java Script, and Node.js...",
-    "website": "https://anhminh10a2hoa.github.io/CV-REACT/",
-    "education": [{
-            "UniversityName": "Hus High School for Gifted Students",
-            "specialization": "Chemistry",
-            "MonthOfPassing": "August",
-            "YearOfPassing": "2018",
-            "Achievements": "Graduated"
-        },
-        {
-            "UniversityName": "Vaasa University of Applied Sciences",
-            "specialization": "Information technology",
-            "MonthOfPassing": "August",
-            "YearOfPassing": "2022",
-            "Achievements": "Present"
-        }
-    ],
-    "experience": [{
-            "CompanyName": "ANZ Solution and Technology Joint Stock Company",
-            "specialization": "PHP & JavaScript Developer",
-            "MonthOfLeaving": "August",
-            "YearOfLeaving": "2019",
-            "Achievements": "Completed"
-        },
-        {
-            "CompanyName": "Vietpro Technology Academy",
-            "specialization": "PHP Web Developer",
-            "MonthOfLeaving": "August",
-            "YearOfLeaving": "2019",
-            "Achievements": "Completed"
-        }
-    ],
-    "skillsDescription": "PROGRAMMING LANGUAGES & TOOLS",
-    "skillLinks": [{
-        "name": "html5",
-        "className": "fab fa-html5"
+  imagebaseurl: "https://rbhatia46.github.io/",
+  name: "Hoang Anh Minh",
+  role: "Software developer",
+  gmail: "hoanganhminhhn17@gmail.com",
+  dob: "25/06/2000",
+  nationality: "Vietnamese",
+  address: "65200 Vaasa, Finland",
+  linkedin: "https://www.linkedin.com/in/minh-hoang-110510181/",
+  phonenumber: "0403620608",
+  roleDescription: "",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/minh-hoang-110510181/",
+      className: "fab fa-linkedin",
     },
     {
-        "name": "css3",
-        "className": "fab fa-css3"
+      name: "github",
+      url: "https://github.com/anhminh10a2hoa",
+      className: "fab fa-github",
     },
     {
-        "name": "php",
-        "className": "fab fa-php"
+      name: "skype",
+      url: "https://www.facebook.com/anhminh.hoang.560",
+      className: "fab fa-facebook-square",
+    },
+  ],
+  aboutme:
+    "Experienced IT developer with a demonstrated history of working in the software industry. Skilled in HTML, CSS, PHP, Java Script, and Node.js...",
+  website: "https://anhminh10a2hoa.github.io/CV-REACT/",
+  education: [
+    {
+      UniversityName: "Hus High School for Gifted Students",
+      specialization: "Chemistry",
+      MonthOfPassing: "August",
+      YearOfPassing: "2018",
+      Achievements: "Graduated",
     },
     {
-        "name": "laravel",
-        "className": "fab fa-laravel"
+      UniversityName: "Vaasa University of Applied Sciences",
+      specialization: "Information technology",
+      MonthOfPassing: "August",
+      YearOfPassing: "2022",
+      Achievements: "Present",
+    },
+  ],
+  experience: [
+    {
+      CompanyName: "ANZ Solution and Technology Joint Stock Company",
+      specialization: "PHP & JavaScript Developer",
+      MonthOfLeaving: "August",
+      YearOfLeaving: "2019",
+      Achievements: "Completed",
     },
     {
-        "name": "nodejs",
-        "className": "fab fa-node"
+      CompanyName: "Vietpro Technology Academy",
+      specialization: "PHP Web Developer",
+      MonthOfLeaving: "August",
+      YearOfLeaving: "2019",
+      Achievements: "Completed",
     },
     {
-        "name": "reactjs",
-        "className": "fab fa-react"
+      CompanyName: "Attract project",
+      specialization: "React Native Developer",
+      MonthOfLeaving: "June",
+      YearOfLeaving: "2020",
+      Achievements: "Completed",
     },
     {
-        "name": "sass",
-        "className": "fab fa-sass"
+      CompanyName: "Project X",
+      specialization: "React Js Developer",
+      MonthOfLeaving: "Current",
+      YearOfLeaving: "2020",
+      Achievements: "In progress",
+    },
+  ],
+  skillsDescription: "PROGRAMMING LANGUAGES & TOOLS",
+  skillLinks: [
+    {
+      name: "aws",
+      className: "fab fa-java",
     },
     {
-        "name": "bootstrap",
-        "className": "fab fa-bootstrap"
+      name: "aws",
+      className: "fab fa-js",
     },
     {
-        "name": "database",
-        "className": "fas fa-database"
+      name: "aws",
+      className: "fab fa-aws",
     },
-    ],
-    "testimonials": [{
-            "name": "Mobile Shop by PHP",
-            "link": "https://github.com/anhminh10a2hoa/ShopByPHP",
-            "image": "./images/ShopByPhp.png"
-        },
-        {
-            "name": "Shopping Web by Laravel",
-            "link": "https://github.com/anhminh10a2hoa/MVC-Laravel",
-            "image": "./images/ShopByLaravel.png"
-        },
-        {
-            "name": "E-book Online by PHP",
-            "link": "https://github.com/anhminh10a2hoa/ebookonline",
-            "image": "./images/eBookOnline.png"
-        },
-        {
-            "name": "Raspberry Pi",
-            "link": "https://github.com/anhminh10a2hoa/appdev1",
-            "image": "./images/rasppi.png"
-        },
-        {
-            "name": "Another projects by NodeJs, ReactJs, ...",
-            "link": "https://github.com/anhminh10a2hoa?tab=repositories",
-            "image": "./images/anotherProject.jpg"
-        }
-    ]
-}
+    {
+      name: "html5",
+      className: "fab fa-html5",
+    },
+    {
+      name: "css3",
+      className: "fab fa-css3",
+    },
+    {
+      name: "php",
+      className: "fab fa-php",
+    },
+    {
+      name: "laravel",
+      className: "fab fa-laravel",
+    },
+    {
+      name: "nodejs",
+      className: "fab fa-node",
+    },
+    {
+      name: "reactjs",
+      className: "fab fa-react",
+    },
+    {
+      name: "sass",
+      className: "fab fa-sass",
+    },
+    {
+      name: "bootstrap",
+      className: "fab fa-bootstrap",
+    },
+    {
+      name: "database",
+      className: "fas fa-database",
+    },
+  ],
+  testimonials: [
+    {
+      name: "Mobile Shop by PHP",
+      link: "https://github.com/anhminh10a2hoa/ShopByPHP",
+      image: "./images/ShopByPhp.png",
+    },
+    {
+      name: "Shopping Web by Laravel",
+      link: "https://github.com/anhminh10a2hoa/MVC-Laravel",
+      image: "./images/ShopByLaravel.png",
+    },
+    {
+      name: "E-book Online by PHP",
+      link: "https://github.com/anhminh10a2hoa/ebookonline",
+      image: "./images/eBookOnline.png",
+    },
+    {
+      name: "Web visualization",
+      link: "",
+      image: "./images/WebVisualization.png",
+    },
+    {
+      name: "Raspberry Pi",
+      link: "https://github.com/anhminh10a2hoa/appdev1",
+      image: "./images/rasppi.png",
+    },
+    {
+      name: "Another projects by NodeJs, ReactJs, ...",
+      link: "https://github.com/anhminh10a2hoa?tab=repositories",
+      image: "./images/anotherProject.jpg",
+    },
+  ],
+};
 
-export default resumeData
+export default resumeData;
